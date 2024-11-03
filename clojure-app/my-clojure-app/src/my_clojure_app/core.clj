4d415100fb6bed1aad3cb4a53f085b3580b7f953
Daniel Rodrigo <maguino@me.com>
2024-11-02 22:34:00 -0500

    Clojure test project

---
+++ clojure-app/my-clojure-app/src/my_clojure_app/core.clj
@@ -0,0 +1,13 @@
+(ns my-clojure-app.core)
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  []
+  (println "Hello, World!"))
+
+(defn greet [name]
+  (str "Hello, " name "!"))
+
+(+ 1 2 3)
+
+(greet "World")
