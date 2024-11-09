dba111b04d80f79f08365e33e4a0a0f086a25944
Daniel Rodrigo <maguino@me.com>
2024-11-09 09:39:38 -0600

    Clojure Kit - Guestbook

---
+++ guestbook/test/clj/yourname/guestbook/core_test.clj
@@ -0,0 +1,8 @@
+(ns yourname.guestbook.core-test
+  (:require
+    [yourname.guestbook.test-utils :as utils]
+    [clojure.test :refer :all]))
+
+(deftest example-test
+  (is (= 1 2)))
+
---
+++ guestbook/test/clj/yourname/guestbook/test_utils.clj
@@ -0,0 +1,16 @@
+(ns yourname.guestbook.test-utils
+  (:require
+    [yourname.guestbook.core :as core]
+    [integrant.repl.state :as state]))
+
+(defn system-state 
+  []
+  (or @core/system state/system))
+
+(defn system-fixture
+  []
+  (fn [f]
+    (when (nil? (system-state))
+      (core/start-app {:opts {:profile :test}}))
+    (f)
+    (core/stop-app)))
